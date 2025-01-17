978167611dcfdf08ca8d31d0ead3bcf8adf2b65f
MEHEDI <md.mehedi0597@gmail.com>
2025-01-17 14:01:10 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_list/home.dart';
+import 'package:to_do_list/provider/todo_provider.dart';
+
+void main(){
+  runApp(MultiProvider(providers: [ChangeNotifierProvider(
+      create: (_)=>TodoProvider())],
+    child: const MyApp(),));
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TO DO List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/provider/todo_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:to_do_list/model/TODOModel.dart';
+
+class TodoProvider extends ChangeNotifier{
+
+
+  final List<TODOModel> _todoList = [];
+  List<TODOModel> get allTODOList => _todoList;
+
+  void  addToDoList(TODOModel todoModel){
+    _todoList.add(todoModel);
+    notifyListeners();
+  }
+  void todoStatusChange(TODOModel todoModel){
+    final index=_todoList.indexOf(todoModel);
+    _todoList[index].toggleCompleted();
+    notifyListeners();
+  }
+
+  void removeToDoList(TODOModel todoModel){
+    final index = _todoList.indexOf(todoModel);
+    _todoList.removeAt(index);
+    notifyListeners();
+  }
+}
