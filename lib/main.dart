476349596c69e8c249b2f64b31092dcc1b9de801
Md. Mehedi Hasan <74109996+Mehedihasan8231@users.noreply.github.com>
2025-01-27 11:09:10 +0600

    Revert "Revert "Branch1""

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_list/home.dart';
+import 'extra_screen.dart';
 import 'package:to_do_list/provider/todo_provider.dart';
 
 void main(){
@@ -20,7 +20,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
       ),
-      home: HomePage(),
+      initialRoute: '/extra_screen',
+      routes: {
+        '/extra_screen':(context)=>ExtraScreen(),
+        '/home':(context)=>HomePage(),
+      },
+      //home: ExtraScreen(),
     );
   }
 }
--- lib/provider/todo_provider.dart
+++ lib/provider/todo_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:to_do_list/model/TODOModel.dart';
 
 class TodoProvider extends ChangeNotifier{
